919763054f0092d55b931392c1621e47b5f1a3c5
UP812199 <32273032+mattbishun5@users.noreply.github.com>
2018-07-23 14:24:59 +0100

    Create index.js

---
+++ public/index.js
@@ -0,0 +1,36 @@
+/* global $ */
+
+'use strict';
+
+$(document).ready(function() {  
+    $('.event').on("dragstart", function (event) {   //finds when dragging an element or text area.
+        var dt = event.originalEvent.dataTransfer;
+        dt.setData('Text', $(this).attr('id'));
+    });
+    $('table td').on("dragenter dragover drop", function (event) {  //dragenter is when dragged area enters desired area.
+        event.preventDefault();                                    //dragover is when element is dragged over an area.
+        if (event.type === 'drop') {
+            var data = event.originalEvent.dataTransfer.getData('Text', $(this).attr('id'));
+            data = $('#' + data).detach();
+            data.appendTo($(this));    //if element is in dropzone then it is added to function.
+
+            $('td').click(function () {
+                $(this).attr("contenteditable"); ///becomes editable
+            });
+        }
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
